d9d383f30045e5a6fead1d7f536a6d4061335ebf
yiyong Z <yz4719@users.noreply.github.com>
2020-02-13 12:54:06 -0500

    2nd

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,15 @@
+// when something with class album-list-cover is clicked, run some code
+$('.Cities').on('click', function() {
+	// pull out the album cover url from the element's attributes
+	var mapUrl = $(this).attr('onclick')
+  // log the album cover url to the console
+  console.log(mapUrl)
 
+  // clear the initial text from the div with id album-art
+  $('#space').empty()
+
+  // set the background-image of the div with id album-art to url(...)
+  // I use a string literal to concatenate 'url(' + albumCoverUrl + ')'
+
+  $('#space').css('background-image', `url(${mapUrl})`)
+})
